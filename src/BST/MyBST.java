cc2f127547b09e4d13cc5eaab1dac3116a433bfb
Magadanov <77725865+Magadanov@users.noreply.github.com>
2021-05-23 21:48:17 +0600

    Update MyBST.java

--- src/BST/MyBST.java
+++ src/BST/MyBST.java
@@ -53,40 +53,71 @@ public class MyBST<K extends Comparable<K>, V> {
         return null;
     }
 
-    public void delete(K key){
+    private void delete(K key, Node root){
         Node current = root;
         int cmp = current.key.compareTo(key);
-        while (cmp != 0){
-            if (cmp > 0){
-                current = current.left;
-            } else current = current.right;
+        if (cmp > 0){
+            delete(current.left.key, current.left);
+        } else if (cmp < 0) {
+            delete(current.right.key, current.right);
+        } else {
+            if(current.left != null && current.right != null){
+                // to find min element of right side
+                Node MinRightCurrent = getRightMin(current.right);
+                current.right = deleteRightMin(current.right);
+                current.key = MinRightCurrent.key;
+                current.value = MinRightCurrent.value;
+                // to find max element of left side
+//                Node MaxLeftCurrent = getLeftMax(current.left);
+//                current.left = deleteLeftMax(current.left);
+//                current.key = MaxLeftCurrent.key;
+//                current.value = MaxLeftCurrent.value;
+            } else if (current.left != null){
+                Node MinRightCurrent = getRightMin(current.left);
+                current.right = deleteRightMin(current.left);
+                current.key = MinRightCurrent.key;
+                current.value = MinRightCurrent.value;
+            } else if (current.right != null){
+                Node MinRightCurrent = getRightMin(current.right);
+                current.right = deleteRightMin(current.right);
+                current.key = MinRightCurrent.key;
+                current.value = MinRightCurrent.value;
+            } else {
+                current.key = null;
+                current.value = null;
+            }
         }
-        System.out.println(current.value);
     }
-//    public void delete(K key){
-//        Node current = root;
-//        delete(key, root);
-//    }
-//
-//    private void delete(K key, Node root){
-//        Node current = this.root;
-//        int cmp = current.key.compareTo(key);
-//        if (cmp > 0){
-//            delete(current.left.key, current.left);
-//        } else if (cmp < 0) {
-//            delete(current.left.key, current.right);
-//        } else {
-//            if(current.left != null && current.right != null){
-////                root = null;
-//                System.out.println("ok1");
-//            } else if (current.left != null){
-//                System.out.println("ok2");
-//            } else if (current.right != null){
-//                System.out.println("ok3");
-//            } else {
-//                current = null;
-//            }
-//        }
-//    }
 
+    private Node getRightMin(Node currentRight){
+        if (currentRight.left == null){
+            return currentRight;
+        }
+        return getRightMin(currentRight.left);
+    }
+
+
+    private Node deleteRightMin(Node currentRight){
+        if (currentRight.left == null){
+            if (currentRight.right != null) return currentRight.right;
+            return null;
+        }
+        return deleteRightMin(currentRight.left);
+    }
+
+    private Node getLeftMax(Node currentLeft){
+        if (currentLeft.right == null){
+            return currentLeft;
+        }
+        return getLeftMax(currentLeft.right);
+    }
+
+
+    private Node deleteLeftMax(Node currentLeft){
+        if (currentLeft.right == null){
+            if (currentLeft.left != null) return currentLeft.left;
+            return null;
+        }
+        return deleteLeftMax(currentLeft.right);
+    }
 }
